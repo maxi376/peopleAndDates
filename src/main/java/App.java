11d9a16405b3936a3cd4fb566b2e2aae727a1552
maxi333 <max.timofte@yahoo.com>
2022-04-09 20:57:59 +0300

    commit 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class App {
 
-	public static void main( String args[] ) {
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter nr of people to be read: ");
 		String str = sc.nextLine();
@@ -13,58 +13,58 @@ public class App {
 		String[] first = new String[n];
 		String[] last = new String[n];
 		Date[] dates = new Date[n];
-		int j=0;
-		int k=0;
-		int maxj=0;//biggest number of family members
-		int control=0;
+		int j = 0;
+		int k = 0;
+		int maxj = 0;//biggest number of family members
+		int control = 0;
 		String aux1;
 		Date auxDate;
 		Date maxDate;
 		//also possible to be implemented with tuplets
 
 		//mask for total number of family members(last name)
-		int[] nrFamilyMembers= new int[n];
-		int[] copyNrFamilyMembers= new int[n];
+		int[] nrFamilyMembers = new int[n];
+		int[] copyNrFamilyMembers = new int[n];
 
 		ArrayList<ArrayList<String>> listFamiliiMenbri
 				= new ArrayList<ArrayList<String>>();//list for members of families ordered by age
-		ArrayList<String> listAux=new ArrayList<String>();//auxiliary arraylist
-		ArrayList<String> listAux2=new ArrayList<String>();//auxiliary arraylist
-		ArrayList<String> listLastName=new ArrayList<String>();//list for unique last names
+		ArrayList<String> listAux = new ArrayList<String>();//auxiliary arraylist
+		ArrayList<String> listAux2 = new ArrayList<String>();//auxiliary arraylist
+		ArrayList<String> listLastName = new ArrayList<String>();//list for unique last names
 
 		int[] markedPeople = new int[n];//folosit pentru a cunoaste care persoane au fost parcurse
 
-		for (int i=0; i<n; i++){
+		for (int i = 0; i < n; i++) {
 			str = sc.next();
 			first[i] = str;
 			str = sc.next();
 			last[i] = str;
-			str= sc.nextLine();
+			str = sc.nextLine();
 			//dates[i]=new SimpleDateFormat("dd/MM/yyyy").parse(str);
 			try {
 				dates[i] = new SimpleDateFormat("yyyy-MM-dd").parse(str);
-			}catch (Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
 		//sort based on dates
-		for (int i=0; i<n-1; i++){
+		for (int i = 0; i < n - 1; i++) {
 			//initializing
-			maxDate=dates[i];
-			j=0;
+			maxDate = dates[i];
+			j = 0;
 
 			//actual ascending sort based on dates
-			for (k=i+1; k<n; k++){
+			for (k = i + 1; k < n; k++) {
 				//the oldest
 				if (maxDate.compareTo(dates[k]) > 0) {
-					maxDate=dates[k];
-					j=k;
+					maxDate = dates[k];
+					j = k;
 				}
 			}
 
 
-			if(maxDate.compareTo(dates[i]) != 0 && j!=i){
+			if (maxDate.compareTo(dates[i]) != 0 && j != i) {
 				aux1 = first[i];
 				first[i] = first[j];
 				first[j] = aux1;
@@ -85,30 +85,29 @@ public class App {
 			System.out.println(dates[i]);
 		}*/ //testing debugging grounds
 
-		for (int i = 0; i < n; i++){
-			nrFamilyMembers[i]=0;
+		for (int i = 0; i < n; i++) {
+			nrFamilyMembers[i] = 0;
 		}
 
 		//caluclating the number of members for each family
-		for (int i = 0; i < n; i++){
-			str=last[i];
-			j=0;
+		for (int i = 0; i < n; i++) {
+			str = last[i];
+			j = 0;
 
-			for (k = 0; k < n; k++){
-				if (str.equals(last[k])){
+			for (k = 0; k < n; k++) {
+				if (str.equals(last[k])) {
 					j++;
 				}
 			}
-			nrFamilyMembers[i]=j;
-			if (j>maxj)
-				maxj=j;
+			nrFamilyMembers[i] = j;
+			if (j > maxj)
+				maxj = j;
 		}
 
 		//copy of nrFamilyMembers
-		for (int i = 0; i < n; i++){
+		for (int i = 0; i < n; i++) {
 			copyNrFamilyMembers[i] = nrFamilyMembers[i];
 		}
-
 
 
 		//control will signal if that person was traversed or not
@@ -117,8 +116,7 @@ public class App {
 
 			for (int i = 0; i < n; i++) {
 				if (copyNrFamilyMembers[i] == j) {
-					if (!listLastName.contains(last[i]))
-					{
+					if (!listLastName.contains(last[i])) {
 						control = 1;//we continue searching for families with j members
 						listLastName.add(last[i]);
 					}
@@ -129,20 +127,20 @@ public class App {
 				j++;
 		}//ascending ordered list of family names
 
-		j=0;
-		for (String s : listLastName){
+		j = 0;
+		for (String s : listLastName) {
 
 			listFamiliiMenbri.add(new ArrayList<String>());
-			listAux=listFamiliiMenbri.get(j);
+			listAux = listFamiliiMenbri.get(j);
 
 			//initializing
 			for (int i = 0; i < n; i++)
-				markedPeople[i]=0;
+				markedPeople[i] = 0;
 
 
 			for (int i = 0; i < n; i++) {
-				k=0;
-				if (last[i].equals(s)){
+				k = 0;
+				if (last[i].equals(s)) {
 					if (markedPeople[i] == 0) {
 
 						k = i;
@@ -154,8 +152,8 @@ public class App {
 						}
 
 						markedPeople[k] = 1;    //marked
-						listAux.add(first[k]);	//adding
-						if (markedPeople[i]==0)
+						listAux.add(first[k]);    //adding
+						if (markedPeople[i] == 0)
 							i--;                //and continue with i not i++
 					}
 				}
@@ -164,12 +162,12 @@ public class App {
 			j++;
 		}
 
-		j=0;
-		for (String s : listLastName){
+		j = 0;
+		for (String s : listLastName) {
 			System.out.print(s + ": ");
 
-			listAux=listFamiliiMenbri.get(j);
-			for (String se :listAux){
+			listAux = listFamiliiMenbri.get(j);
+			for (String se : listAux) {
 				System.out.print(se + " ");
 			}
 			System.out.println();
